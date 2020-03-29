2dab7146766e28f6bc06824686ad9e9e588e2573
Attol8 <jacopoattolini@gmail.com>
2020-03-29 22:53:49 +0200

    addedd comments

--- coffee_machine/coffee_machine.py
+++ coffee_machine/coffee_machine.py
@@ -1,11 +1,19 @@
 # Write your code here
 class CoffeeMachine:
-    def __init__(self):  
-        self.water = 400
-        self.milk = 540
-        self.beans = 120
-        self.money = 550
-        self.disposable_cups= 9
+    """A simple representation of a coffe machine.
+
+    :param water: An int, the coffe machine's available water
+
+    :param milk: An int, the coffe machine's available milk
+    :param beans: An int, the coffe machine's available coffe beans
+    :param money: An int, the coffe machine's collected money
+    """
+    def __init__(self, water, milk, beans, money, disposable_cups): 
+        self.water = water
+        self.milk = milk
+        self.beans = beans
+        self.money = money
+        self.disposable_cups= disposable_cups
         self.all_resources = [self.water, self.milk, self.beans, self.disposable_cups]
         self.coffee_type = None
         self.current_state = 'action' 
@@ -24,12 +32,14 @@ class CoffeeMachine:
             }
         
     def set_new_state(self, command):
+        """ A function that change the coffee machine's state based on user's command"""
         try:
             self.current_state = self.command_to_state[command]
         except KeyError:
             self.current_state = 'input_error'
 
     def receive_input(self, command):
+        """A function that handle the user's input that comes to it"""
         while True:
             if self.current_state == 'action':
                 self.set_new_state(command)
@@ -61,10 +71,12 @@ class CoffeeMachine:
             break
     
     def input_error(self, command):
+        """if There is an input error in the command, this function returns an errorr message"""
         print("The command you typed '{0}' has not been recognized by the coffee machine".format(command))
         print()
 
     def check_resources(self, coffee_type):
+        """Check if the coffee machine ha enough resources to make the required beverage. If it doesn't, Return the scarce resource"""
         check =  [x - y for x, y in zip(self.all_resources, self.required_resources[self.coffee_type])]
         if min(check) >= 0: return None
         else:
@@ -75,7 +87,10 @@ class CoffeeMachine:
             if check.index(min(check)) == 0 : return 'disposable cups'
             
     def make_coffee(self, coffee_type):
+        """Receives user's input about the type of coffee. Then check if there are enoug resources to make this type of coffee and make it
 
+        :param coffe_type: An int (or 'back') input by user , 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu. 
+        """
         if coffee_type in ['1' , '2' , '3']: 
             self.coffee_type = int(coffee_type)
         elif coffee_type == 'back' :
@@ -98,6 +113,10 @@ class CoffeeMachine:
         print()
         
     def fill(self, resource_fill):
+        """A function that fills the coffee machine. The user decides the amount of resources he/she wants to fill the mchine with
+
+        :param resource_fill: A list, the fill amounts of all the resources (chosen by user) [water, milk, beans, disposable_cups]
+        """        
         print()
         self.water = self.water + resource_fill[0]
         self.milk = self.milk + resource_fill[1]
@@ -107,11 +126,13 @@ class CoffeeMachine:
         print()
 
     def take(self):
+        """A function that takes all the money collected so far by the coffee machine and give it to the user """
         print('I gave you {0}'.format(self.money))
         self.money = 0
         print()
 
     def remaining(self):
+        """A function that prints all the remaining resources in the coffee machine"""
         print()
         print('The coffee machine has:')
         print('{0} of water'.format(self.water))
@@ -123,7 +144,7 @@ class CoffeeMachine:
 
 def main():
     #main program
-    coffee_machine = CoffeeMachine()
+    coffee_machine = CoffeeMachine(400, 540, 120, 9, 550)
     while coffee_machine.active == True:
         coffee_machine.current_state = 'action'
         command = str(input('Write action (buy, fill, take, remaining, exit):'))
